a34d7acbc6cba93c51cb47bb7a6b0c4f52a094eb
Ankita2426 <ankitakapoor1526@gmail.com>
2020-07-18 16:44:25 +0530

    importing npm modules

--- index.js
+++ index.js
@@ -10,6 +10,11 @@
 // //console.log(nam);
 // const sum = add(6,8);
 // console.log(sum);
+const validator = require('validator')
 const getnotes = require('./utils.js')
-const notes = getnotes();
+const notes = getnotes("your notes are here");
 console.log(notes);
+console.log(validator.isEmail("ankita@gmail.com"));
+console.log(validator.isEmail("gmail.com"));//this packsge return true if something is valid and false if something is not valid    
+//to check url
+console.log(validator.isURL("https://pen.io"));
