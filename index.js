e8b4d5d3946fbaa2246c44c51b9304809ec96d6c
Ankita2426 <ankitakapoor1526@gmail.com>
2020-07-18 14:48:38 +0530

    importing own files

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-//console.log("hello ");
-fs.writeFileSync("notes.txt" , "this will be awesome " )// <--just by doing this we are not going to contain any output
-//append
-fs.appendFileSync("notes.txt" ," this is created by nodejs !")
+// //console.log("hello ");
+// fs.writeFileSync("notes.txt" , "this will be awesome " )// <--just by doing this we are not going to contain any output
+// //append
+// fs.appendFileSync("notes.txt" ," this is created by nodejs !")
+
+//  const add = require('./utils.js')
+// //const name = 'ankita';
+// //console.log(nam);
+// const sum = add(6,8);
+// console.log(sum);
+const getnotes = require('./utils.js')
+const notes = getnotes();
+console.log(notes);
---
+++ utils.js
@@ -0,0 +1,14 @@
+console.log("utils.js");
+//practise
+// const name = 'kapoor';
+// module.exports = name;
+//func
+//const add = function(a,b){
+  //  return a+b;
+//}
+//module.exports = add
+//test
+const getnotes = function(a){
+    return " your notes..";
+}
+module.exports = getnotes
