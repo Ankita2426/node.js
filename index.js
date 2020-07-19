c11815155026cbc21220a8c2a44def893707f4aa
Ankita2426 <ankitakapoor1526@gmail.com>
2020-07-19 08:53:54 +0530

    getting ip from user

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const chalk = require('chalk')
+const getnotes = require('./utils.js')
+const command = process.argv[2];
+console.log(process.argv);
+if(command === "add"){
+    console.log('adding notes');
+}else if(command === "remove"){
+    console.log("statement is removed");
+}else {
+    console.log("nothing happens");
+}
+
+
+
 // const fs = require('fs')
 
 // //console.log("hello ");
@@ -11,13 +25,16 @@
 // const sum = add(6,8);
 // console.log(sum);
 //const validator = require('validator')
-const getnotes = require('./utils.js')
-const notes = getnotes("your notes are here");
-console.log(notes);
+// const getnotes = require('./utils.js')
+// const notes = getnotes("your notes are here");
+// console.log(notes);
 //console.log(validator.isEmail("ankita@gmail.com"));
 //console.log(validator.isEmail("gmail.com"));//this packsge return true if something is valid and false if something is not valid    
 //to check url
 //console.log(validator.isURL("https://pen.io"));
-const chalk = require('chalk')
-console.log(chalk.yellow.bgRed.inverse.bold('warning'));
-console.log(chalk.blue.bgRed.inverse.bold('warning'));  //inplace of node index.js write nodemon index.js and these changes will occur autimaticall
+//const chalk = require('chalk')
+// console.log(chalk.yellow.bgRed.inverse.bold('warning'));
+// console.log(chalk.blue.bgRed.inverse.bold('warning'));  //inplace of node index.js write nodemon index.js and these changes will occur autimaticall
+// //getting ip from user
+// //console.log(process.argv);
+// console.log(process.argv[2]);
