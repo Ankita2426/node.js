a3f653c7bb3d3dfe9b2937247aed57d67fee4851
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-03 18:03:43 +0530

    error handling in weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,42 @@
 const request = require('request')
 // // const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363"
 // //units are also added
-// const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363&units=f"
+// //const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363&units=f"
+// const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=&units=f"
 
 // request({url: url, json: true}, (error,response) => {
+//     if(error){
+//         console.log("unable to connect to weather service. please check your internet connection.");
+//     }else if(response.body.error){
+//         console.log("unable to find location");
+//     }else{
+//         console.log(response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
+//     }
+//   //  console.log(error);
 // //    // console.log(response);
 // //    const data = JSON.parse(response.body)
 // //    console.log(data.current);
 // //console.log(response.body.current);
 //   //  console.log("it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
-//   console.log(response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
-// })
+//  })
 
 
 
 //challenge
 
-const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw&limit=1"
-
+//const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw&limit=1"
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/georgia.json?types=country&access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw"
 request({url: url, json: true}, (error,response) => {
-     const long = response.body.features[0].center[0]
-     const lat = response.body.features[0].center[1]
-     console.log(long,lat);
+    if(error){
+                console.log("unable to connect to weather service. please check your internet connection.");
+            }else if(response.body.features.length===0){
+                console.log("unable to find location,try another result");
+            }else{
+                const long = response.body.features[0].center[0]
+                const lat = response.body.features[0].center[1]
+                console.log(long,lat);
+            }
+    
     })
     
     
@@ -43,4 +58,4 @@ request({url: url, json: true}, (error,response) => {
 // }, 0)   
 // //here 0 second timer is printed after stopping. because some internals of nodejs are resposible for it
 
-// console.log("stopping");
+// console.log("stopping")
