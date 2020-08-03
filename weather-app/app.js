b8394450365c84fb91d93a1a0fbdd9c7c748d7cd
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-03 13:31:41 +0530

    making http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,24 @@
-console.log("starting");
+const request = require('request')
+const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363"
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000)
+request({url: url}, (error,response) => {
+   // console.log(response);
+   const data = JSON.parse(response.body)
+   console.log(data.current);
+})
 
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0)   
-//here 0 second timer is printed after stopping. because some internals of nodejs are resposible for it
 
-console.log("stopping");
+// console.log("starting");
+
+// setTimeout(() => {
+//     console.log('2 second timer');
+// }, 2000)
+
+
+// setTimeout(() => {
+//     console.log('0 second timer');
+// }, 0)   
+// //here 0 second timer is printed after stopping. because some internals of nodejs are resposible for it
+
+// console.log("stopping");
