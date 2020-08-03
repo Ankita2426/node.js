e75e3e7423d68a93e3aa04d2b42c6c6eeaf6abbe
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-03 17:24:48 +0530

    mapbox,challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,33 @@
 const request = require('request')
-// const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363"
-//units are also added
-const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363&units=f"
+// // const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363"
+// //units are also added
+// const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363&units=f"
+
+// request({url: url, json: true}, (error,response) => {
+// //    // console.log(response);
+// //    const data = JSON.parse(response.body)
+// //    console.log(data.current);
+// //console.log(response.body.current);
+//   //  console.log("it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
+//   console.log(response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
+// })
+
+
+
+//challenge
+
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw&limit=1"
 
 request({url: url, json: true}, (error,response) => {
-//    // console.log(response);
-//    const data = JSON.parse(response.body)
-//    console.log(data.current);
-//console.log(response.body.current);
-  //  console.log("it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
-  console.log(response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " and its feel like " + response.body.current.feelslike);
-})
+     const long = response.body.features[0].center[0]
+     const lat = response.body.features[0].center[1]
+     console.log(long,lat);
+    })
+    
+    
+
+
+
 
 
 
