404c5b00dba7d1e44829721abde15439d2c2adc3
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-06 12:27:51 +0530

    call abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 // // const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363"
 // //units are also added
 // //const url = "http://api.weatherstack.com/current?access_key=e966ff17ea74b9ab332a581b8117fb0f&query=32.1109,%2076.5363&units=f"
@@ -24,24 +25,26 @@ const request = require('request')
 
 //challenge
 
-//const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw&limit=1"
-const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/georgia.json?types=country&access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw"
-request({url: url, json: true}, (error,response) => {
-    if(error){
-                console.log("unable to connect to weather service. please check your internet connection.");
-            }else if(response.body.features.length===0){
-                console.log("unable to find location,try another result");
-            }else{
-                const long = response.body.features[0].center[0]
-                const lat = response.body.features[0].center[1]
-                console.log(long,lat);
-            }
+// //const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw&limit=1"
+// const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/georgia.json?types=country&access_token=pk.eyJ1IjoiYW5raXRhMjQyNiIsImEiOiJja2RlZjBtcWgwbGNjMnFucm1sOTNmOHhsIn0.Og8ohhqSwSZMQAEiLSElAw"
+// request({url: url, json: true}, (error,response) => {
+//     if(error){
+//                 console.log("unable to connect to weather service. please check your internet connection.");
+//             }else if(response.body.features.length===0){
+//                 console.log("unable to find location,try another result");
+//             }else{
+//                 const long = response.body.features[0].center[0]
+//                 const lat = response.body.features[0].center[1]
+//                 console.log(long,lat);
+//             }
     
-    })
-    
-    
+//     })
 
 
+geocode('12what', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
 
 
@@ -59,3 +62,5 @@ request({url: url, json: true}, (error,response) => {
 // //here 0 second timer is printed after stopping. because some internals of nodejs are resposible for it
 
 // console.log("stopping")
+
+
