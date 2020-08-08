cf954af2d27ce6bfae44fb95777cd8ee1bc29f81
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-08 20:05:12 +0530

    serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,30 +1,60 @@
 const express = require('express')
+//app.com
+//app.com/help
+//app.com/about
 
 const app = express()
 
+// app.get('' , (req,res) => {
+//     res.send('hello express')
+
+// })
+
+
+// app.get('/help' , (req,res) => {
+//     res.send('help page')
+
+// })
+//sending html
+
 app.get('' , (req,res) => {
-    res.send('hello express')
+    res.send('<h1>over all is a good person</h1>')
 
 })
 
-
+//json
 app.get('/help' , (req,res) => {
-    res.send('help page')
+    res.send({
+        name: "Dev",
+        age: 21,
+        height: 175,
+        good_qualities: "calm",
+        bad: "doesn't understand",
+
+    })
 
 })
 
 app.get('/about' , (req,res) => {
-    res.send('about us ')
+    res.send('but i m angry ')
 
 })
 
 app.get('/weather' , (req,res) => {
-    res.send('here is the weather')
+    res.send('i dont wanna talk')
 
 })
-//app.com
-//app.com/help
-//app.com/about
+
 app.listen(3000 , () =>{
     console.log("server is up on port 3000");
+})
+
+//array
+app.get('/array' , (req,res) => {
+    res.send([{
+        name: 'Ankita'
+    },{
+        name: "kapoor"
+    } ])
+
 })
