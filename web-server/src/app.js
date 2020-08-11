7a8c3ebbd05aea376020d26e591a0291cadb1caf
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-11 18:17:46 +0530

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -80,7 +80,24 @@ app.get('/about' , (req,res) => {
 })
 
 app.get('/weather' , (req,res) => {
-    res.send('i dont wanna talk')
+    res.send({
+        forecast: 'it is sunny',
+        location: 'palampur'
+    })
+
+})
+
+app.get('/address' , (req,res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'you must provide address'
+        })
+    }
+    res.send({
+        forecast: 'it is sunny',
+        location: 'palampur',
+        address: req.query.address      
+    })
 
 })
 app.get('/weather/*' , (req,res) => {
@@ -90,6 +107,21 @@ app.get('/weather/*' , (req,res) => {
     name: "Ankita",
     errormessage: 'weather not found'
 })
+
+})
+
+app.get('/products' , (req,res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'you must provide a search term'
+        })
+    }
+    console.log(req.query); 
+    console.log(req.query.search);
+    res.send({
+        
+        products: []
+    })
 
 })
 
