d4f237d3c1c73d60e95a978d5f836549940485c3
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-11 23:08:57 +0530

    browser http req with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
-console.log("client site js file is loaded");
+console.log("client site js file is loaded")
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/address?address=palampur').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error);
+        } else{
+            console.log(data.location)
+            console.log(data.forecast);
+        }
+    })
+})
+
