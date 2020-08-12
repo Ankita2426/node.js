bf7a585c262fe4e2fe3048a489e0b1b5182b6faf
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-12 13:57:37 +0530

    wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,18 +7,25 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne  = document.querySelector('#message-1')
+const messagetwo  = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit' , (e) => {
     e.preventDefault()
     const location = search.value
+    messageOne.textContent = 'loading...'
+    messagetwo.textContent = ''
     
 fetch('http://localhost:3000/address?address= ' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            console.log(data.error);
+       //     console.log(data.error);
+       messageOne.textContent = data.error
         } else{
-            console.log(data.location)
-            console.log(data.forecast);
+            // console.log(data.location)
+            // console.log(data.forecast);
+            messageOne.textContent = data.location
+            messagetwo.textContent = data.forecast
         }
     })
 })
