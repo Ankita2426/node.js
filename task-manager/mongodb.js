437d48e0fbacd2ca15e28adc7927dd0830090e53
Ankita2426 <ankitakapoor1526@gmail.com>
2020-08-18 23:57:20 +0530

    updating data

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -21,48 +21,41 @@ MongoClient.connect(connectionURL , { useUnifiedTopology : true } , (error , cli
     }
   //  console.log('connected correctly!');
     const db = client.db(databaseName)
-    
-    //FINDONE
-// //SEARCH BY NAME AND AGE
+    //  //UPDATE USE FILTER
 
-//     // db.collection('users').findOne({ name: "Ankita" , age: 19}, (error ,user) => {
-//         //SEARCH BY ID
+    //  const updatePromise = db.collection('users').updateOne({
+    //      _id: new ObjectID("5f3b8da5086556455c3d9abd")
+    //  }, {
+    //     //  $set: {
+    //     //      name: "Hardeep"
+    //     //  }
+    //     $inc:{
+    //         age: 2
+    //     }
+    //  })
+    //  updatePromise.then((result) =>{
+    //      console.log(result);
+    //  }).catch((error)=> {
+    //     console.log(error);
+    //  })
 
-//         db.collection('users').findOne({ _id: new ObjectID("5f3b8da5086556455c3d9abd")}, (error ,user) => { 
-//         // db.collection('users').findOne({ _id: "5f3b8da5086556455c3d9abd"}, (error ,user) => {//it will return null because its not string its a binary data we need to give objectId
-//         if(error){
-//             console.log("unable to fetch")
-//         }
-//         console.log(user);
-//     })
+    //challenge (under task)   update many
 
-//FIND
-//by searching with id it wound be nonsense because id is unique
-    db.collection('users').find({age: 19}).toArray((error , users) =>{
-        console.log(users);
-    })
-
-    db.collection('users').find({age: 19}).count((error , users) =>{
-        console.log(users);
-    })
-
-
-
-//challenge
-            //findone
-    db.collection('Tasks').findOne({ _id: new ObjectID("5f3b8f4287730f279c0c57da")} , (error , user) =>{
-        console.log(user);
-    })
-
-    //find
-    db.collection('Tasks').find({completed: "False"}).toArray((error,userss) =>{
-        console.log(userss);
-    })
-
+    db.collection("Tasks").updateMany({
+        completed: "False"
+    } ,{
+        $set:{
+            completed: "True"
+        }
+    }).then((result) =>{
+         console.log(result.modifiedCount);
+     }).catch((error)=> {
+        console.log(error);
+     })
 
 })
 
-
+            //create
 
     //     db.collection('users').insertOne({
     //     _id: id,
@@ -121,3 +114,43 @@ MongoClient.connect(connectionURL , { useUnifiedTopology : true } , (error , cli
 //     }
 //     console.log(result.ops);
 //     })
+
+
+    
+//     //FINDONE READ
+// // //SEARCH BY NAME AND AGE
+
+// //     // db.collection('users').findOne({ name: "Ankita" , age: 19}, (error ,user) => {
+// //         //SEARCH BY ID
+
+// //         db.collection('users').findOne({ _id: new ObjectID("5f3b8da5086556455c3d9abd")}, (error ,user) => { 
+// //         // db.collection('users').findOne({ _id: "5f3b8da5086556455c3d9abd"}, (error ,user) => {//it will return null because its not string its a binary data we need to give objectId
+// //         if(error){
+// //             console.log("unable to fetch")
+// //         }
+// //         console.log(user);
+// //     })
+
+// //FIND
+// //by searching with id it wound be nonsense because id is unique
+// db.collection('users').find({age: 19}).toArray((error , users) =>{
+//     console.log(users);
+// })
+
+// db.collection('users').find({age: 19}).count((error , users) =>{
+//     console.log(users);
+// })
+
+
+
+// //challenge
+//         //findone
+// db.collection('Tasks').findOne({ _id: new ObjectID("5f3b8f4287730f279c0c57da")} , (error , user) =>{
+//     console.log(user);
+// })
+
+// //find
+// db.collection('Tasks').find({completed: "False"}).toArray((error,userss) =>{
+//     console.log(userss);
+// })
+
